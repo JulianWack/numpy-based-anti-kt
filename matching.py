764244c5d49f405aef1db4aadf30720323895a3f
JulianWack <86099747+JulianWack@users.noreply.github.com>
2021-08-12 17:05:18 +0100

    upload files from swan projects

---
+++ matching.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def matching(etas1, phis1, etas2, phis2):
+    '''Match jets with etas2, phis2 (recojets) to jets with etas1, phis1 (myjets), s.t. distance in eta, phi plane (d = sqrt((eta1-eta2)**2 + (phi1-phi2)**2)) is small.
+    An array is returned where the element at position i gives the id of the jet2 (recojet) matched to the i-th jet1 (myjet). Priority is given to matching hard jets first.
+    When the element is -1, then the considered jet1 (myjet) is assumend to be fake (not a real jet, hence no corresponding jet2 (genjet) exists). 
+    When the minimal available distance is larger than 0.2, jet1 is assumed to be fake. Justification for this cut off is given in "DeltaR_cutoff" notebook.'''
+    R_cutoff = 0.2
+    my_matching = np.full(len(etas1), -1, dtype='int8') # default value of -1 means that all unmatched myjets will be regarded as fake jets
+    
+    for i, (eta1, phi1) in enumerate(zip(etas1, phis1)):
+        # find distance between one of jet1 (myjets) and all jet2 (genjets)
+        Rs = np.sqrt((eta1-etas2)**2 + (phi1-phis2)**2)
+        
+        mins_idx = np.argsort(Rs)
+        for idx in mins_idx:
+            # when index already used in an earlier pair, go the index providing next smallest distance
+            if idx in my_matching:
+                continue
+            # when difference larger than max difference for correct matching, claim jet to be fake and leave entry as -1 
+            elif Rs[idx] >= R_cutoff:
+                break
+            else:
+                my_matching[i] = idx
+                break
+                
+    return my_matching
---
+++ np_4vect_sum.py
@@ -0,0 +1,82 @@
+import numpy as np
+
+
+def CosTheta(x, y, z):
+    '''Find angle theta between z axis and cartesian 3 vector'''
+    mag = np.sqrt(x**2+y**2+z**2)
+    if mag == 0.0:
+        costheta = 1
+    else:
+        costheta = z/mag
+        
+    return costheta
+
+
+def get_pt(x, y):
+    '''Find pt as polar radial distance from z axis'''
+    return np.sqrt(x**2+y**2)
+
+
+def get_eta(x, y, z):
+    '''Find pesudo rapidity from cartesian 3 vector'''
+    cosTheta = CosTheta(x, y, z)
+    if cosTheta**2 < 1:
+        eta = -0.5*np.log((1.0-cosTheta)/(1.0+cosTheta))
+    elif z == 0:
+        eta = 0
+    elif z > 0:
+        eta = 10e10
+    else:
+        eta = -10e10
+        
+    return eta
+
+
+def get_phi(x, y):
+    '''Find polar angle phi from cartesian 4 vector'''
+    if (x==0) and (y==0):
+        phi = 0
+    else:
+        phi = np.arctan2(y,x)
+        
+    return phi
+
+
+def get_m(x, y, z, t):
+    '''Find invariant mass from cartesian 4 vector'''
+    delta_s2 = t**2 - (x**2 + y**2 + z**2)
+    if delta_s2 < 0:
+        m = -np.sqrt(-delta_s2)
+    else:
+        m = np.sqrt(delta_s2)
+        
+    return m
+
+
+def np_sum4vec(pt, eta, phi, mass):
+    '''Takes in 4 1D np.arrays as componnents of len(pt)-many PtEtaPhiM 4 vectors and returns the sum of these 4 vectors
+    as a np.array of length 4 with elements in order of: Pt, Eta, Phi, Mass.'''
+    
+    # make 4 vector based on ROOT TLorentzVector::SetPtEtaPhiM, which allows element wise sumation
+    n_PF = len(pt)
+    vec4 = np.ndarray((n_PF,4))
+    
+    vec4[:,0] = pt*np.cos(phi)
+    vec4[:,1] = pt*np.sin(phi)
+    vec4[:,2] = pt*np.sinh(eta)
+    
+    non_neg = np.sqrt(pt**2*np.cosh(eta)**2 + mass**2)
+    neg = np.sqrt(np.maximum(pt**2*np.cosh(eta)**2 - mass**2, 0))
+    vec4[:,3] = np.where(mass>=0, non_neg, neg)
+
+    vec4_summed = np.sum(vec4, axis=0)
+    
+    # extract pt, eta, phi, mass from cartesian 4 vector based on ROOT TLorentzVector::Pt, Eta, Phi, M
+    sum_PtEtaPhiM = np.empty(4)
+    sum_PtEtaPhiM[0] = get_pt(vec4_summed[0], vec4_summed[1])
+    sum_PtEtaPhiM[1] = get_eta(vec4_summed[0], vec4_summed[1], vec4_summed[2])
+    sum_PtEtaPhiM[2] = get_phi(vec4_summed[0], vec4_summed[1])
+    sum_PtEtaPhiM[3] = get_m(vec4_summed[0], vec4_summed[1], vec4_summed[2], vec4_summed[3])
+    
+    
+    return sum_PtEtaPhiM
