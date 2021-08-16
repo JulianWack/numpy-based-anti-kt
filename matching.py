34e27d3279d733dee74fc8ff99418c0429898359
JulianWack <86099747+JulianWack@users.noreply.github.com>
2021-08-16 14:27:45 +0100

    working with R squared rather than R

--- matching.py
+++ matching.py
@@ -5,20 +5,20 @@ def matching(etas1, phis1, etas2, phis2):
     An array is returned where the element at position i gives the id of the jet2 (recojet) matched to the i-th jet1 (myjet). Priority is given to matching hard jets first.
     When the element is -1, then the considered jet1 (myjet) is assumend to be fake (not a real jet, hence no corresponding jet2 (genjet) exists). 
     When the minimal available distance is larger than 0.2, jet1 is assumed to be fake. Justification for this cut off is given in "DeltaR_cutoff" notebook.'''
-    R_cutoff = 0.2
+    R2_cutoff = 0.2**2
     my_matching = np.full(len(etas1), -1, dtype='int8') # default value of -1 means that all unmatched myjets will be regarded as fake jets
     
     for i, (eta1, phi1) in enumerate(zip(etas1, phis1)):
         # find distance between one of jet1 (myjets) and all jet2 (genjets)
-        Rs = np.sqrt((eta1-etas2)**2 + (phi1-phis2)**2)
+        R2s = (eta1-etas2)**2 + (phi1-phis2)**2
         
-        mins_idx = np.argsort(Rs)
+        mins_idx = np.argsort(R2s)
         for idx in mins_idx:
             # when index already used in an earlier pair, go the index providing next smallest distance
             if idx in my_matching:
                 continue
             # when difference larger than max difference for correct matching, claim jet to be fake and leave entry as -1 
-            elif Rs[idx] >= R_cutoff:
+            elif R2s[idx] >= R2_cutoff:
                 break
             else:
                 my_matching[i] = idx
